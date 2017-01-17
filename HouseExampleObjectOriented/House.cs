b05f3721198101cb44846d9698911c36f61606f5
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-17 14:28:27 -0500

    added two new public methods

--- HouseExampleObjectOriented/House.cs
+++ HouseExampleObjectOriented/House.cs
@@ -8,5 +8,19 @@ namespace HouseExampleObjectOriented
 {
     class House
     {
+        private string color; //declaring fields in a class
+        private int rooms;
+        private int size;
+        private int doors;
+
+        public void Paint(string color) //creating  a method, public = anyone can modify it, void = won't return anything
+        {
+            this.color = color; //"this" will call back to the variable in the class above
+        }
+        public string CurrentColor()
+        {
+            return color;
+        }
+
     }
 }
