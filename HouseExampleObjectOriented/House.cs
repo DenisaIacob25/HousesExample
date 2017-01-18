bc5e0c0f5cca246c39e271d6325d4032117d18ea
Denisa Iacob <denisaiacob18@gmail.com>
2017-01-18 14:25:07 -0500

    final commit for practice probelm

--- HouseExampleObjectOriented/House.cs
+++ HouseExampleObjectOriented/House.cs
@@ -8,11 +8,44 @@ namespace HouseExampleObjectOriented
 {
     class House
     {
-        private string color; //declaring fields in a class
+        private string color; //declaring fields in a class either public or private
         private int rooms;
         private int size;
         private int doors;
 
+        private string name;
+
+        //properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //constructors
+        public House()
+        {
+            this.color = "Grey";
+            this.rooms = 2;
+            this.size = 300;
+            this.doors = 1;
+        }
+
+        public House(string color, int size)
+        {
+            this.color = color;
+            this.size = size;
+        }
+
+        public House(string color, int rooms, int size, int doors)
+        {
+            this.color = color;
+            this.rooms = rooms;
+            this.size = size;
+            this.doors = doors;
+        }
+
+        //methods
         public void Paint(string color) //creating  a method, public = anyone can modify it, void = won't return anything
         {
             this.color = color; //"this" will call back to the variable in the class above
@@ -20,6 +53,13 @@ namespace HouseExampleObjectOriented
         public string CurrentColor()
         {
             return color;
+        }
+        public void DisplayHouseInfo()
+        {
+            Console.Write("House Color: " + color);
+            Console.Write(", # of rooms: " + rooms);
+            Console.Write(", size is: " + size + "sq/ft");
+            Console.WriteLine(", with: " + doors + " doors.");
         }
 
     }
--- HouseExampleObjectOriented/Program.cs
+++ HouseExampleObjectOriented/Program.cs
@@ -10,6 +10,35 @@ namespace HouseExampleObjectOriented
     {
         static void Main(string[] args)
         {
+            House house1 = new House("green", 4, 1000, 3);
+            House house2 = new House("blue", 700);
+            House house3 = new House();
+
+            house1.DisplayHouseInfo();
+            house2.DisplayHouseInfo();
+            house3.DisplayHouseInfo();
+
+            house2.Paint("Yellow");
+            house2.DisplayHouseInfo();
+
+            house2.Name = "The blue house";
+            Console.WriteLine(house2.Name); //outputting the value of the property 
+            
+            //house1.Paint("Green");
+            //house1.NumberofRooms(4);
+            //house1.SetSize(1000);
+            //house1.NumberOfDoors(3);
+
+
+            //Console.WriteLine(house1.CurrentColor());
+
+            //house1.Paint("White");
+            //Console.WriteLine(house1.CurrentColor());
+
+            //House house2 = new House();
+            //house2.Paint("Blue");
+            //Console.WriteLine("House one is: " + house1.CurrentColor());
+            //Console.WriteLine("House two is: " + house2.CurrentColor());
         }
     }
 }
